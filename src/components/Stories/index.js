52697de5af1fba8de75ae1a08933f33f1684b829
415CA <chrisallen415@gmail.com>
2020-12-15 11:56:12 -0500

    added stories header

--- src/components/Stories/index.js
+++ src/components/Stories/index.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import './css/index.css';
 import Story from '../Story';
+import StoriesHeader from './StoriesHeader';
+
+const COLUMNS = {
+  title: {
+    label: 'Title',
+    width: '40%',
+  },
+  author: {
+    label: 'Author',
+    width: '30%',
+  },
+  comments: {
+    label: 'Comments',
+    width: '10%',
+  },
+  points: {
+    label: 'Points',
+    width: '10%',
+  },
+  archive: {
+    width: '10%',
+  },
+};
 
 const Stories = ({ stories }) =>
   <div className="stories">
+    <StoriesHeader columns={COLUMNS} />
+
     {(stories || []).map(story =>
       <Story
         key={story.objectID}
         story={story}
+        columns={COLUMNS}
       />
     )}
   </div>
