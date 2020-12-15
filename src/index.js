43c05eff2a1be54b5d2e874bee276c38afc202f5
415CA <chrisallen415@gmail.com>
2020-12-15 13:30:30 -0500

    added store subscribe for re-render

--- src/index.js
+++ src/index.js
@@ -7,18 +7,18 @@ import { STORY_ARCHIVE } from './constants/actionTypes';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App 
+const render = () => {
+  ReactDOM.render(
+    <App
       stories={getReadableStories(store.getState())}
       onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
     />,
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    document.getElementById('root')
+  );
+};
 
-console.log(store.getState())
-console.log(getReadableStories(store.getState()))
+store.subscribe(render);
+render();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
